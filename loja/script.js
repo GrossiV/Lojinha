4aba65b0fa71157ea86c2a26038c9fb7b026bb09
Victor Grossi <victor.grossi@balko.com.br>
2020-08-20 20:04:57 -0300

    add first function

--- loja/script.js
+++ loja/script.js
@@ -1,11 +1,10 @@
 function adicionarNoCarrinho(nomeDoItem) {
   console.log(nomeDoItem)
+  contadorDeProdutos = document.getElementById('numero')
+  console.log(contadorDeProdutos)
+  contadorDeProdutos.innerHTML = 1
 }
 
 function removerDoCarrinho() {
   alert('item removido KEKW')
-}
-
-function AdicionarNoCarro() {
-  console.log("ferrari")
 }
